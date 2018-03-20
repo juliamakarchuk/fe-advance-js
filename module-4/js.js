9232a0c7e3a17de58615b74897ad7727f959ba0d
juliamakarchuk <juliamakarchuk366@gmail.com>
2018-03-20 14:38:03 +0200

    added new js file

--- module-4/js.js
+++ module-4/js.js
@@ -1,26 +1,20 @@
 const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 function addKeyboardLayout(alphabet){
-	const keyboard=alphabet.split("");
-    const array=[];
-    const firstRow=[keyboard.slice(0, keyboard.indexOf('a'))];
-    const secondRow=[keyboard.slice(keyboard.indexOf('a'),keyboard.indexOf('z'))];
-    const thirdRow=[keyboard.slice(keyboard.indexOf('z'))];
-    array.push(firstRow);
-    array.push(secondRow);
-    array.push(thirdRow);
-	return Array.from(array);
+	const arr=alphabet.split("");
+    const keyboard = [arr.slice(0, arr.indexOf("a")), arr.slice(arr.indexOf("a"), arr.indexOf("z")), arr.slice(arr.indexOf("z"))];
+	return keyboard;
 }
 console.log(addKeyboardLayout(alphabet));
 
-var alpha=addKeyboardLayout(alphabet);
+const alpha=addKeyboardLayout(alphabet);
  function getRandCharInRow(row){
-    var keyboardLayout = alpha[row - 1].join(' ');
-    var randLetterLayout=keyboardLayout[Math.floor(Math.random()*(keyboardLayout.length))];
+    let randLetterLayout=alpha[row-1][Math.floor(Math.random()*(alpha[row-1].length))];
     return randLetterLayout;
  }
 console.log(`случайное число первой строки:   ${getRandCharInRow(1)}`);
  function getRandCharInAlph(){
-    var randLetter=alphabet.charAt(Math.floor(Math.random()*alphabet.length));
+    let randRow = Math.floor(Math.random()*alpha.length);
+    let randLetter=alpha[randRow][Math.floor(Math.random()*alpha[randRow].length)];
     return randLetter;
  }
 console.log(`случайное число:   ${getRandCharInAlph()}`);
