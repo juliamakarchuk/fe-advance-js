426b2e9c9ab2ec4aa79b4302387db27761a6aaba
juliamakarchuk <33657533+juliamakarchuk@users.noreply.github.com>
2018-04-25 13:07:30 +0300

    added spaces

--- module-2/js.js
+++ module-2/js.js
@@ -5,18 +5,18 @@ let userInput = prompt(`Введите пароль`);
 for (n = 2; n >= 0; n--) {
   if (passwords.includes(userInput)){
     alert(`Добро пожаловать!`);
-	break;
-	}
+    break;
+  }
   else if (userInput === null) {
-	break;
-	}
+    break;
+  }
   else if (n > 0) {
     alert(`Неверный пароль, у вас осталось ${n} попыток`);
-	userInput = prompt(`Vvedite parol`);
-	}
+    userInput = prompt(`Введите пароль`);
+  }
   else if (n === 0) {
     alert(`У вас закончились попытки, аккаунт заблокирован!`);
-   }
+  }
 }
 
 
